0cf04e8dc7c4086872a4ccb3604e5cfd04074143
Alberto Souza <alberto.souza.99@gmail.com>
2012-11-16 00:13:17 -0200

    template da conexao adicionado

---
+++ plugins/themes/conexao_internacional/CIThemePlugin.inc.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @file DesertThemePlugin.inc.php
+ *
+ * Copyright (c) 2012 Alberto Souza, Anne camille
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class CSecThemePlugin
+ * @ingroup plugins_themes_tema_csec
+ *
+ * @brief "tema_csec" theme plugin
+ */
+
+//$Id$
+
+import('classes.plugins.ThemePlugin');
+
+class CIThemePlugin extends ThemePlugin {
+	/**
+	 * Get the name of this plugin. The name must be unique within
+	 * its category.
+	 * @return String name of plugin
+	 */
+	function getName() {
+		return 'CIThemePlugin';
+	}
+
+	function getDisplayName() {
+		return '1ª Conexão Internacional de Saúde e (Ciber) Cultura';
+	}
+
+	function getDescription() {
+		return '1ª Conexão Internacional de Saúde e (Ciber) Cultura layout';
+	}
+
+	function getStylesheetFilename() {
+		return 'styles.css';
+	}
+
+	function getLocaleFilename($locale) {
+		return null; // No locale data
+	}
+
+}
+
+// EXIBIR O CONTEÚDO DE UMA VARIAVÉL NO CONSOLE DO JAVASCRIPT DO NAVEGADOR
+function debugaa($algo) {
+  $algo = json_encode($algo);
+  print "<script>console.log($algo)</script>";
+}
+
+?>
