8cc817d7417ec71f77a20776c4d3dc3411910c11
Alberto Souza <alberto.souza.99@gmail.com>
2012-11-19 04:53:38 -0200

    texto com tipo de sistema no footer alterado

--- relatorios/footer.php
+++ relatorios/footer.php
@@ -4,7 +4,7 @@
       <strong>VERSÂO 0.1 BETA</strong> Os relatorios ainda estão em desenvolvimento e 
       mais informações serão adicionadas nas proximas atualizações.   
     </p>
-    <p class="muted credit">Página de relatórios desenvolvida pelo 
+    <p class="muted credit">Página de dados desenvolvida pelo 
        <a href="http://www.albertosouza.net">Alberto Souza.</a>
         da equipe <a href="http://www.next.icict.fiocruz.br/redesocial/next">NEXT</a> para o projeto 
         <a href="http://www.next.icict.fiocruz.br/sec/">Rede Saúde e Cultura</a>
