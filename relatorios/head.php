a4a291e4c1836be1fad64ff07baa042f442540e1
Alberto Souza <alberto.souza.99@gmail.com>
2012-11-20 03:57:26 -0200

    Google analitcs adicionado

--- relatorios/head.php
+++ relatorios/head.php
@@ -4,6 +4,8 @@
     <!-- jquery -->
     <script src="http://code.jquery.com/jquery-latest.js"></script>
 
+    <?php include_once( 'analyticstracking.php' ); ?>
+
     <!-- Bootstrap -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet"> 
